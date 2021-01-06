3a47f5c10155d3e8ec5852e5cf76279ece583c9c
Paridhi Modi <54037847+paridhimodi@users.noreply.github.com>
2021-01-06 12:31:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ from __future__ import division, print_function
 import os
 
 import numpy as np
-from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from keras.models import load_model
+from keras.preprocessing import image
 
 from flask import Flask, redirect, url_for, request, render_template
 from werkzeug.utils import secure_filename
